27c5b28cbbc6b4b00228ef312f3b2e6bdf9964ae
ShahriarRaktim <shahriarahmedraktim@gmail.com>
2021-11-25 18:17:40 +0600

    update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ import "./Home.css";
 
 const Home = () => {
   const [services, setServices] = useState([]);
-  const {isloading} =useAuth();
+  const { isloading } = useAuth();
   useEffect(() => {
     fetch("https://secret-plateau-40724.herokuapp.com/plans")
       .then((res) => res.json())
@@ -15,9 +15,7 @@ const Home = () => {
   }, []);
   return (
     <>
-    {
-      isloading && <Spinner animation="border" />
-    }
+      {isloading && <Spinner animation="border" />}
       {/* Banar Section */}
       <section className="banar">
         <h1>
@@ -37,7 +35,10 @@ const Home = () => {
             <h1>MyWORLD</h1>
             <h2>Complete Preparation Here For Enjoying A Tour</h2>
             <p>
-            Traveling is a quintessential part of enriching our lives and building out new memories. You see new sites, try new foods, hear new languages, and learn new things which lead to new positive memories.
+              Traveling is a quintessential part of enriching our lives and
+              building out new memories. You see new sites, try new foods, hear
+              new languages, and learn new things which lead to new positive
+              memories.
             </p>
             <div className="all-info">
               <div className="info">
@@ -47,19 +48,24 @@ const Home = () => {
                 <div>
                   <h4>Quality of Care Services</h4>
                   <p>
-                  The Tour Guy we believe that living a long life is more than how many years pass you by. We believe the best way to live a long life is to accrue as many memories as possible.
+                    The Tour Guy we believe that living a long life is more than
+                    how many years pass you by. We believe the best way to live
+                    a long life is to accrue as many memories as possible.
                   </p>
                 </div>
               </div>
 
               <div className="info">
                 <h3>
-                <i class="fas fa-plane"></i>
+                  <i class="fas fa-plane"></i>
                 </h3>
                 <div>
                   <h4>Standards of Plane</h4>
                   <p>
-                  No single country or event can take credit for evolving the arts but the Renaissance in Florence brought it back to life in Europe. Navigate Europe’s top museums to help plan your trip.
+                    No single country or event can take credit for evolving the
+                    arts but the Renaissance in Florence brought it back to life
+                    in Europe. Navigate Europe’s top museums to help plan your
+                    trip.
                   </p>
                 </div>
               </div>
@@ -91,9 +97,9 @@ const Home = () => {
           <h1>Management Service</h1>
           <h2>Our MyWORLD Service</h2>
           <p>
-            As you continue to keep tabs on the <span>MyWORLD</span>{" "}
-            industry, don’t forget to prioritize the deep-seated passion you’ve
-            been feeling to propel your career forward.
+            As you continue to keep tabs on the <span>MyWORLD</span> industry,
+            don’t forget to prioritize the deep-seated passion you’ve been
+            feeling to propel your career forward.
           </p>
         </div>
         <Row xs={1} md={2} lg={3} className="m-0 g-4">
@@ -122,9 +128,14 @@ const Home = () => {
           </p>
 
           <h4>
-          We booked a flight with this airline for myself and family members in 3 months advance. Just 10 days prior to the departure, I received an email stating that the airline has canceled the flight. So I had to book that same route flight almost 3 times costlier than the previously booked one. Never Book flight with this airline.Time is the continued sequence of existence and events that
-            occurs in an apparently irreversible succession from the past,
-            through the present
+            We booked a flight with this airline for myself and family members
+            in 3 months advance. Just 10 days prior to the departure, I received
+            an email stating that the airline has canceled the flight. So I had
+            to book that same route flight almost 3 times costlier than the
+            previously booked one. Never Book flight with this airline.Time is
+            the continued sequence of existence and events that occurs in an
+            apparently irreversible succession from the past, through the
+            present
           </h4>
         </div>
         <div className="review">
@@ -141,9 +152,15 @@ const Home = () => {
           </p>
 
           <h4>
-          Awesome!I called flight care they offered they have one flight at 3.30 am if you can make it we can book We booked a flight with this airline for myself and family members in 3 months advance. Just 10 days prior to the departure, I received an email stating that the airline has canceled the flight. So I had to book that same route flight almost 3 times costlier than the previously booked one. Never Book flight with this airline.Time is the continued sequence of existence and events that
-            occurs in an apparently irreversible succession from the past,
-            through the present
+            Awesome!I called flight care they offered they have one flight at
+            3.30 am if you can make it we can book We booked a flight with this
+            airline for myself and family members in 3 months advance. Just 10
+            days prior to the departure, I received an email stating that the
+            airline has canceled the flight. So I had to book that same route
+            flight almost 3 times costlier than the previously booked one. Never
+            Book flight with this airline.Time is the continued sequence of
+            existence and events that occurs in an apparently irreversible
+            succession from the past, through the present
           </h4>
           <NavLink to="/home" className="contact">
             More Reviews <i class="fas fa-arrow-alt-circle-right"></i>
